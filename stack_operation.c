321b0da9d4f2645d7d8bdc1fd11c95ec0c6ff314
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-08-26 14:57:46 +0000

    Update pint function

--- stack_operation.c
+++ stack_operation.c
@@ -62,9 +62,13 @@ void pall(stack_t **stack, unsigned int line_number)
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
-	if (stack == NULL || *stack == NULL)
+	if (stack == NULL)
 		return;
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
 	fprintf(stdout, "%d\n", (*stack)->n);
 }
