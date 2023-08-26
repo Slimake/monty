317a85db3383136e652d5675d5327346a45f37ae
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-08-26 14:38:40 +0000

    Add pint function to header file

--- monty.h
+++ monty.h
@@ -44,5 +44,6 @@ typedef struct instruction_s
 /* User-defined functions*/
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
