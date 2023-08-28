c6ef142c11622c58c571a55e71838f05de31454d
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-08-28 12:24:40 +0000

    Add newline character

--- stack_operation.c
+++ stack_operation.c
@@ -87,7 +87,7 @@ void pop(stack_t **stack, unsigned int line_number)
 		return;
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pop an empty stack", line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
