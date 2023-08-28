883175f81ba7f556374e03e7023a330c7d8dbac8
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-08-28 10:34:48 +0000

    Add free_stack function

--- monty.h
+++ monty.h
@@ -45,5 +45,6 @@ typedef struct instruction_s
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *stack);
 
 #endif /* MONTY_H */
