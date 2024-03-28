883084b897bb098a144aad4bf0c5e09e4cc1a7f0
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-03-28 17:25:04 +0000

    Entry into Monty program

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
+/**
+ * main - entry point into the program
+ * @argc: command-line argument counter
+ * @argv: array of pointers(an array containing command-line arguments)
+ *
+ * Return: an integer
+ */
+
+int main(int argc, char *argv[])
+{
+	monty(argc, argv);
+
+	return (0);
+}
